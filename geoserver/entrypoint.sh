4fedecbe3ff8287281e0a57422c556e7c56f82c7
Martin Eder <mail@martin-eder.eu>
2026-03-02 19:20:17 +0100

    Add workaround for geoserver credentials

--- geoserver/entrypoint.sh
+++ geoserver/entrypoint.sh
@@ -12,19 +12,7 @@ sed -i "s|__POSTGRES_PASSWORD__|${POSTGRES_PASSWORD}|g" $GEOSERVER_DATA_DIR/work
 
 # Pass admin credentials from env
 
-GEOSERVER_ADMIN_USER="${GEOSERVER_ADMIN_USER:-admin}"
-GEOSERVER_ADMIN_PASS="${GEOSERVER_ADMIN_PASS:-geoserver}"
-
-USERS_FILE="$GEOSERVER_DATA_DIR/security/usergroup/default/users.properties"
-ROLES_FILE="$GEOSERVER_DATA_DIR/security/usergroup/default/roles.properties"
-
-mkdir -p "$(dirname "$USERS_FILE")"
-mkdir -p "$(dirname "$ROLES_FILE")"
-
-if ! grep -q "^$GEOSERVER_ADMIN_USER=" "$USERS_FILE" 2>/dev/null; then
-  echo "Creating GeoServer admin user..."
-  echo "$GEOSERVER_ADMIN_USER=$GEOSERVER_ADMIN_PASS,ROLE_ADMIN" > "$USERS_FILE"
-  echo "$GEOSERVER_ADMIN_USER=ROLE_ADMIN" > "$ROLES_FILE"
-fi
+sed -i "s|__GEO_USER__|${GEOSERVER_ADMIN_USER}|g" $GEOSERVER_DATA_DIR/security/usergroup/default/users.xml
+sed -i "s|__GEO_PASSWORD__|${GEOSERVER_ADMIN_PASSWORD}|g" $GEOSERVER_DATA_DIR/security/usergroup/default/users.xml
 
 exec "$@"
