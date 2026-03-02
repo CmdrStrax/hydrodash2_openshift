1bca52639243309cd807e6023d500d2a9ff1a21e
Martin Eder <mail@martin-eder.eu>
2026-03-02 17:36:53 +0100

    Geoserver adapt entrypoint script

--- geoserver/entrypoint.sh
+++ geoserver/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 sed -i "s|__POSTGRES_DB__|${POSTGRES_DB}|g" $GEOSERVER_DATA_DIR/workspaces/hydrodash/hydrodash/datastore.xml
 sed -i "s|__POSTGRES_HOST__|${POSTGRES_HOST}|g" $GEOSERVER_DATA_DIR/workspaces/hydrodash/hydrodash/datastore.xml
 sed -i "s|__POSTGRES_PORT__|${POSTGRES_PORT}|g" $GEOSERVER_DATA_DIR/workspaces/hydrodash/hydrodash/datastore.xml
